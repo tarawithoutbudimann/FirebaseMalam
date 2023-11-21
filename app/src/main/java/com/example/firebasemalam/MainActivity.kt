232e2cf3ebd140838f36d4cbbb5c3bb8dcf1131f
tarawithoutbudimann <tamarasashikirana@mail.ugm.ac.id>
2023-11-21 10:00:53 +0700

    first commit

---
+++ app/src/main/java/com/example/firebasemalam/MainActivity.kt
@@ -0,0 +1,138 @@
+package com.example.firebasemalam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.lifecycle.MutableLiveData
+import com.example.firebasemalam.databinding.ActivityMainBinding
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MainActivity : AppCompatActivity() {
+    private val firestore = FirebaseFirestore.getInstance()
+    private val budgetCollectionRef = firestore.collection("budgets")
+    private lateinit var binding: ActivityMainBinding
+    private var updateId = ""
+    private val budgetListLiveData: MutableLiveData<List<Budget>> by lazy {
+        MutableLiveData<List<Budget>>()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            btnAdd.setOnClickListener {
+                val nominal = edtNominal.text.toString()
+                val description = edtDesc.text.toString()
+                val date = edtDate.text.toString()
+                val newBudget = Budget(nominal = nominal, description = description,
+                    date = date)
+                addBudget(newBudget)
+            }
+
+            btnUpdate.setOnClickListener {
+                val nominal = edtNominal.text.toString()
+                val description = edtDesc.text.toString()
+                val date = edtDate.text.toString()
+                val budgetToUpdate = Budget(nominal = nominal, description =
+                description, date = date)
+                updateBudget(budgetToUpdate)
+                updateId = ""
+                resetForm()
+            }
+
+            listView.setOnItemClickListener { adapterView, _, i, _ ->
+                val item = adapterView.adapter.getItem(i) as Budget
+                updateId = item.id
+                edtNominal.setText(item.nominal)
+                edtDesc.setText(item.description)
+                edtDate.setText(item.date)
+            }
+
+            listView.onItemLongClickListener = AdapterView.OnItemLongClickListener {
+                    adapterView, _, i, _ ->
+                val item = adapterView.adapter.getItem(i) as Budget
+                deleteBudget(item)
+                true
+            }
+        }
+        observeBudgets()
+        getAllBudgets()
+    }
+
+    private fun getAllBudgets() {
+        observeBudgetChanges()
+    }
+
+    private fun observeBudgets() {
+        budgetListLiveData.observe(this) { budgets ->
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                budgets.toMutableList()
+            )
+            binding.listView.adapter = adapter
+        }
+    }
+
+    private fun observeBudgetChanges() {
+        budgetCollectionRef.addSnapshotListener { snapshots, error ->
+            if (error != null) {
+                Log.d("MainActivity", "Error listening for budget changes: ", error)
+                return@addSnapshotListener
+            }
+            val budgets = snapshots?.toObjects(Budget::class.java)
+            if (budgets != null) {
+                budgetListLiveData.postValue(budgets)
+            }
+        }
+    }
+
+    private fun addBudget(budget: Budget) {
+        budgetCollectionRef.add(budget)
+            .addOnSuccessListener { documentReference ->
+                val createdBudgetId = documentReference.id
+                budget.id = createdBudgetId
+                documentReference.set(budget)
+                    .addOnFailureListener {
+                        Log.d("MainActivity", "Error updating budget ID: ", it)
+                    }
+                resetForm()
+            }
+            .addOnFailureListener {
+                Log.d("MainActivity", "Error adding budget: ", it)
+            }
+    }
+
+    private fun resetForm(){
+        with(binding){
+            edtNominal.setText("")
+            edtDesc.setText("")
+            edtDate.setText("")
+        }
+    }
+
+    private fun updateBudget(budget: Budget) {
+        budget.id = updateId
+        budgetCollectionRef.document(updateId).set(budget)
+            .addOnFailureListener {
+                Log.d("MainActivity", "Error updating budget: ", it)
+            }
+    }
+
+    private fun deleteBudget(budget: Budget) {
+        if (budget.id.isEmpty()) {
+            Log.d("MainActivity", "Error deleting: budget ID is empty!")
+            return
+        }
+        budgetCollectionRef.document(budget.id).delete()
+            .addOnFailureListener {
+                Log.d("MainActivity", "Error deleting budget: ", it)
+            }
+    }
+
+
+}
